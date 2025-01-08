91b15a606081950b38921bd0f9a0f9c4ede8b9f1
eswar2001 <eswar.tadiparth@juspay.in>
2025-01-08 13:58:54 +0530

    fixed cli --help

--- src/parser/Language/Haskell/Tools/Parser/ProgramOptions.hs
+++ src/parser/Language/Haskell/Tools/Parser/ProgramOptions.hs
@@ -4,6 +4,7 @@
 module Language.Haskell.Tools.Parser.ProgramOptions where
 
 import           Options.Applicative
+import           Control.Monad (void)
 
 data Options =
   Options
@@ -17,70 +18,69 @@ data ModuleAction = MERGE | REMOVE | MODIFY
   deriving (Show)
 
 data ParseConfig = Refact ASTParseConfig | SplitAndWrite WriteFileConfig | Module ModuleConfig
+  deriving (Show)
 
 data ModuleConfig =
-  ModuleConfig {
-    sourceModulePath :: String
-    , sourceModuleName :: String
+  ModuleConfig
+    { sourceModulePath    :: String
+    , sourceModuleName    :: String
     , destinationModulePath :: String
     , destinationModuleName :: String
-    , moduleAction :: ModuleAction
-  }
+    , moduleAction        :: ModuleAction
+    }
   deriving (Show)
 
 data ASTParseConfig =
   ASTParseConfig
-    { modulePath      :: String
-    , moduleName      :: String
-    , functionality   :: ASTRefactors
+    { modulePath    :: String
+    , moduleName    :: String
+    , functionality :: ASTRefactors
     }
   deriving (Show)
-
 
 data WriteFileConfig =
   WriteFileConfig
-    { funDeps      :: String
-    , clusters     :: String
-    , modPath      :: String
-    , newModName   :: String
-    , modName      :: String
+    { funDeps        :: String
+    , clusters       :: String
+    , modPath        :: String
+    , newModName     :: String
+    , modName        :: String
     }
   deriving (Show)
 
-
+-- The main options parser
 opts :: Parser ParseConfig
-opts =
-      subparser (command "getFunDeps" $ (info (parserConfig FunctionDependency) (progDesc "Get the function dependency graph of a module")))
-  <|> subparser (command "letRefactor" $ (info (parserConfig FunctionDependency) (progDesc "Multiple Let Refactoring")))
-  <|> subparser (command "mergeModule" $ (info (modifyModuleConfig MERGE) (progDesc "Merge source to destination")))
-  <|> subparser (command "removeModule" $ (info (modifyModuleConfig REMOVE) (progDesc "Remove source from destination")))
-  <|> subparser (command "modifyModule" $ (info (modifyModuleConfig MODIFY) (progDesc "modify source in destination")))
-  <|> subparser (command "writeModSplits" $ (info writeFileConfig (progDesc "Split the grouped functions into modules")))
+opts = subparser
+  (  command "getFunDeps"     (info (parserConfig FunctionDependency) (progDesc "Get the function dependency graph of a module"))
+  <> command "letRefactor"     (info (parserConfig LetRefactoring) (progDesc "Multiple Let Refactoring"))
+  <> command "removeWildCards" (info (parserConfig RemoveWildCards) (progDesc "Remove wildcards in a module"))
+  <> command "mergeModule"     (info (modifyModuleConfig MERGE) (progDesc "Merge source to destination"))
+  <> command "removeModule"    (info (modifyModuleConfig REMOVE) (progDesc "Remove source from destination"))
+  <> command "modifyModule"    (info (modifyModuleConfig MODIFY) (progDesc "Modify source in destination"))
+  <> command "writeModSplits"  (info writeFileConfig (progDesc "Split the grouped functions into modules"))
+  )
 
+-- Subparser for modifying module configurations
 modifyModuleConfig :: ModuleAction -> Parser ParseConfig
-modifyModuleConfig action =
-  Module
-  <$> (ModuleConfig
-  <$> (strOption (long "source-module-path" <> help "path to which module to read changes from"))
-  <*> (strOption (long "destination-module-path" <> help "path to which module to apply changes to"))
-  <*> (strOption (long "source-module-name" <> help "module name to read changes from"))
-  <*> (strOption (long "destination-module-name" <> help "module name to apply changes to"))
-  <*> (pure action))
+modifyModuleConfig action = Module <$> (ModuleConfig
+  <$> strOption (long "source-module-path" <> help "Path to read changes from")
+  <*> strOption (long "source-module-name" <> help "Name of the source module")
+  <*> strOption (long "destination-module-path" <> help "Path to apply changes to")
+  <*> strOption (long "destination-module-name" <> help "Name of the destination module")
+  <*> pure action)
 
+-- Subparser for refactoring options (function dependency, let refactoring, etc.)
 parserConfig :: ASTRefactors -> Parser ParseConfig
-parserConfig refactor =
-      Refact
-  <$> (ASTParseConfig
-  <$> (strOption (long "module-path" <> short 'p' <> help "Path of module to be split"))
-  <*> (strOption (long "module-name" <> short 'o' <> help "Module Name"))
-  <*> (pure refactor))
+parserConfig refactor = Refact <$> (ASTParseConfig
+  <$> strOption (long "module-path" <> short 'p' <> help "Path of the module to refactor")
+  <*> strOption (long "module-name" <> short 'o' <> help "Module name")
+  <*> pure refactor)
 
+-- Parser for splitting and writing modules
 writeFileConfig :: Parser ParseConfig
-writeFileConfig =
-  SplitAndWrite
-  <$> (WriteFileConfig
-  <$> (strOption (long "fun-deps" <> short 'l' <> help "List of function dependencies"))
-  <*> (strOption (long "groups" <> short 'g' <> help "Grouped Functions list"))
-  <*> (strOption (long "module-path" <> short 'p' <> help "Path of module to be split"))
-  <*> (strOption (long "new-die" <> short 'o' <> help "New directory name for the module"))
-  <*> (strOption (long "module-name" <> short 'o' <> help "Module Name")))
+writeFileConfig = SplitAndWrite <$> (WriteFileConfig
+  <$> strOption (long "fun-deps" <> short 'l' <> help "Function dependencies")
+  <*> strOption (long "groups" <> short 'g' <> help "Grouped functions")
+  <*> strOption (long "module-path" <> short 'p' <> help "Path to the module to split")
+  <*> strOption (long "new-die" <> short 'o' <> help "New directory name for the module")
+  <*> strOption (long "module-name" <> short 'o' <> help "Module name"))
--- src/parser/exe/Main.hs
+++ src/parser/exe/Main.hs
@@ -12,17 +12,46 @@ import Data.Functor (($>), void)
 
 main :: IO ()
 main = do
-    parserComb <- execParser (info opts idm)
-    case parserComb of
-        Module moduleConfig -> do
-            print moduleConfig
-            performAction moduleConfig
-        Refact astParseConfig ->
-            case functionality astParseConfig of
-                FunctionDependency -> do
-                    deps <- getFunctionDeps (modulePath astParseConfig) (moduleName astParseConfig)
-                    print deps
-                    pure ()
-                LetRefactoring -> removeMultiLets (modulePath astParseConfig) (moduleName astParseConfig) $>  ()
-                RemoveWildCards -> removeWildCards (modulePath astParseConfig) (moduleName astParseConfig) $>  ()
-        SplitAndWrite writeFileConfig -> void (splitAndWrite (modPath writeFileConfig) (modName writeFileConfig) (clusters writeFileConfig) (funDeps writeFileConfig))
+    parserComb <- execParser (info (opts <**> helper) (fullDesc))
+    handleCommand parserComb
+
+-- Handling different command cases
+handleCommand :: ParseConfig -> IO ()
+handleCommand (Module moduleConfig) = handleModuleAction moduleConfig
+handleCommand (Refact astParseConfig) = handleRefactor astParseConfig
+handleCommand (SplitAndWrite writeFileConfig) = handleSplitAndWrite writeFileConfig
+
+-- Handling module actions
+handleModuleAction :: ModuleConfig -> IO ()
+handleModuleAction moduleConfig = do
+  print moduleConfig
+  performAction moduleConfig
+
+-- Handling refactor actions
+handleRefactor :: ASTParseConfig -> IO ()
+handleRefactor astParseConfig = case functionality astParseConfig of
+  FunctionDependency -> getAndPrintFunctionDeps astParseConfig
+  LetRefactoring    -> refactorLet astParseConfig
+  RemoveWildCards   -> removeWildcards astParseConfig
+
+-- Get function dependencies and print them
+getAndPrintFunctionDeps :: ASTParseConfig -> IO ()
+getAndPrintFunctionDeps astParseConfig = do
+  deps <- getFunctionDeps (modulePath astParseConfig) (moduleName astParseConfig)
+  print deps
+
+-- Perform let refactoring
+refactorLet :: ASTParseConfig -> IO ()
+refactorLet astParseConfig = removeMultiLets (modulePath astParseConfig) (moduleName astParseConfig) $> ()
+
+-- Remove wildcards
+removeWildcards :: ASTParseConfig -> IO ()
+removeWildcards astParseConfig = removeWildCards (modulePath astParseConfig) (moduleName astParseConfig) $> ()
+
+-- Handle the split and write functionality
+handleSplitAndWrite :: WriteFileConfig -> IO ()
+handleSplitAndWrite writeFileConfig = void $ splitAndWrite 
+  (modPath writeFileConfig)
+  (modName writeFileConfig)
+  (clusters writeFileConfig)
+  (funDeps writeFileConfig)
